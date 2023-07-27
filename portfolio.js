0e0a226dfa07b002e6c9b2ef8448acd1926285fd
Zain Muhammad <zain.muhammad@oladoc.com>
2023-07-27 20:51:52 +0500

    second commit

---
+++ portfolio.js
@@ -0,0 +1,51 @@
+function openNav() {
+    var element = document.getElementById("nav-mobile");
+    element.classList.toggle("active");
+}
+
+// code for change the specialities
+const symbols = ['Web Design Specialist', 'UI Automation Expert'];
+let count = 0;
+const element = document.getElementById("change");
+const iteration = () => {
+    element.innerHTML = symbols[parseInt(count / 2, 10) % symbols.length] + ' .';
+    if (count % 2 !== 0) {
+        element.classList.add("out");
+    } else {
+        element.classList.remove("out");
+    }
+    count++;
+    // if you're affraid the code could run for so long that it could fail,
+    // 6 is enough (number of elems * 2)
+    if (count === symbols.length * 2) {
+        count = 0;
+    }
+};
+let inthandle = setInterval(iteration, 1000);
+iteration();
+
+// <div id="change">Web Design Specialist</div>
+// #change {
+//     transition: transform 0.5s;
+//     display: inline-block;
+//     }
+//     .out {
+//         transform: scale(0);
+//     }
+// code for change the specialities
+
+
+/* cursor js */
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
+/* cursor js */
